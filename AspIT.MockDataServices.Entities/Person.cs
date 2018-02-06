460ec64b5953db850d5b2f845bca7d2e2391dce9
jesp6763 <jesp6763@edu.campusvejle.dk>
2018-02-06 10:40:17 +0100

    Uploaded project

---
+++ AspIT.MockDataServices.Entities/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.MockDataServices.Entities
+{
+    public class Person
+    {
+        #region [Fields]
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
+        protected string firstName;
+
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
+        protected string lastName;
+        #endregion
+
+        #region [Constructors]
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        #endregion
+
+        #region [Properties]
+        /// <summary>
+        /// Gets or sets the firstname.
+        /// </summary>
+        public string FirstName { get => firstName; set => firstName = value; }
+
+        /// <summary>
+        /// Gets or sets the lastname.
+        /// </summary>
+        public string LastName { get => lastName; set => lastName = value; }
+        #endregion
+    }
+}
---
+++ AspIT.MockDataServices/PersonJsonService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using AspIT.MockDataServices.Entities;
+
+namespace AspIT.MockDataServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PersonJsonService : IPersonJsonService
+    {
+        public string GetAllPeople()
+        {
+            // TODO: Refactor when done testing
+            string connectionString = @"Data Source = CVDB3, 1444; Initial Catalog = MockDataDB; Integrated Security = True;";
+            string sql = "SELECT FirstName,LastName FROM DanishNames";
+            DataSet dataSet = new DataSet();
+
+            // Connect to database
+            using(SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand(sql, new SqlConnection(connectionString))))
+            {
+                adapter.Fill(dataSet);
+            }
+
+            string jsonPeople = string.Empty;
+            DataRowCollection rows = dataSet.Tables[0].Rows;
+            Person[] people = new Person[rows.Count];
+
+            for(int i = 0; i < rows.Count; i++)
+            {
+                DataRow row = rows[i];
+                people[i] = new Person(row.Field<string>("FirstName"), row.Field<string>("LastName"));
+            }
+
+            jsonPeople = JsonConvert.SerializeObject(people);
+
+            // Return json data
+            return jsonPeople;
+        }
+    }
+}
