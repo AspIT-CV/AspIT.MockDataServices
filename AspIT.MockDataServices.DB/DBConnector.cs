7291b8d45cc21a7a48b84bbb63e45483a0ba0a89
jesp6763 <jesp6763@edu.campusvejle.dk>
2018-02-06 11:08:35 +0100

    Added DB Project

---
+++ AspIT.MockDataServices.DB/DBConnector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.MockDataServices.DB
+{
+    public class DBConnector
+    {
+    }
+}
