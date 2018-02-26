ba9bc0bdae4e1ee0952f9f4cff212b26b8a91373
jesp6763 <jesp6763@edu.campusvejle.dk>
2018-02-26 13:37:32 +0100

    Removed unused using directive

--- AspIT.MockDataServices.DB/SqlExecutor.cs
+++ AspIT.MockDataServices.DB/SqlExecutor.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
-using System.Web.Configuration;
 
 namespace AspIT.MockDataServices.DB
 {
