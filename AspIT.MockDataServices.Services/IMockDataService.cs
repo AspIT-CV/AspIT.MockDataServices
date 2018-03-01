98811e71c88f720c823d404a050c9d6d8fcc2e58
jesp6763 <jesp6763@edu.campusvejle.dk>
2018-03-01 13:37:20 +0100

    Renamed the service, and added the ability to get addresses

--- AspIT.MockDataServices.Services/IMockDataService.cs
+++ AspIT.MockDataServices.Services/IMockDataService.cs
@@ -10,7 +10,7 @@ namespace AspIT.MockDataServices.Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IMockNamesJsonService
+    public interface IMockDataService
     {
         /// <summary>
         /// Selects all names from the DanishNames table, and then returns the names in JSON format.
@@ -26,5 +26,20 @@ namespace AspIT.MockDataServices.Services
         /// <param name="amount">How many names to select.</param>
         [OperationContract]
         string GetDanishNames(int amount);
+
+        /// <summary>
+        /// Gets all addresses from the Addresses table in the database, and then returns it in JSON format.
+        /// </summary>
+        /// <returns>Addresses in JSON format</returns>
+        [OperationContract]
+        string GetAllAddresses();
+
+        /// <summary>
+        /// Gets the specified amount of addresses from the Addresses table in the database, and then returns it in JSON format.
+        /// </summary>
+        /// <param name="amount">The amount of addresses to get. If the amount is more than what is in the table, then only that amount will be retreived.</param>
+        /// <returns>Addresses in JSON format</returns>
+        [OperationContract]
+        string GetAddresses(int amount);
     }
 }
---
+++ AspIT.MockDataServices.Services/MockDataService.svc.cs
@@ -0,0 +1,130 @@
+﻿using System.Data;
+using Newtonsoft.Json;
+using AspIT.MockDataServices.Entities;
+using AspIT.MockDataServices.DB;
+using System.ServiceModel;
+
+namespace AspIT.MockDataServices.Services
+{
+    /// <summary>
+    /// Represents a WCF Service that is responsible for returning stuff in JSON format/>
+    /// </summary>
+    public class MockDataService : IMockDataService
+    {
+        /// <summary>
+        /// Selects all names from the DanishNames table, and then returns the names in JSON format.
+        /// </summary>
+        /// <returns>The names in JSON format</returns>
+        public string GetAllDanishNames()
+        {
+            SqlExecutor sqlExecutor = new SqlExecutor();
+            DataSet dataSet = sqlExecutor.Execute("SELECT FirstName,LastName FROM DanishNames");
+
+            string jsonPeople = string.Empty;
+            DataRowCollection rows = dataSet.Tables[0].Rows;
+            Person[] people = new Person[rows.Count];
+
+            for(int i = 0; i < rows.Count; i++)
+            {
+                DataRow row = rows[i];
+                people[i] = new Person(row.Field<string>("FirstName"), row.Field<string>("LastName"));
+            }
+
+            jsonPeople = JsonConvert.SerializeObject(people);
+
+            // Return json data
+            return jsonPeople;
+        }
+
+        /// <summary>
+        /// Selects the amount of names specified from the DanishNames table, and then returns the names in JSON format.
+        /// </summary>
+        /// <returns>The names in JSON format</returns>
+        /// <param name="amount">How many names to select.</param>
+        public string GetDanishNames(int amount)
+        {
+            SqlExecutor sqlExecutor = new SqlExecutor();
+            DataSet dataSet = sqlExecutor.Execute($"SELECT TOP {amount} FirstName,LastName FROM DanishNames");
+
+            string jsonPeople = string.Empty;
+            DataRowCollection rows = dataSet.Tables[0].Rows;
+            Person[] people = new Person[rows.Count];
+
+            for(int i = 0; i < rows.Count; i++)
+            {
+                DataRow row = rows[i];
+                people[i] = new Person(row.Field<string>("FirstName"), row.Field<string>("LastName"));
+            }
+
+            jsonPeople = JsonConvert.SerializeObject(people);
+
+            // Return json data
+            return jsonPeople;
+        }
+
+        /// <summary>
+        /// Gets all addresses from the Addresses table in the database, and then returns it in JSON format.
+        /// </summary>
+        /// <returns>Addresses in JSON format</returns>
+        public string GetAllAddresses()
+        {
+            SqlExecutor sqlExecutor = new SqlExecutor();
+            DataSet dataSet = sqlExecutor.Execute($"SELECT * FROM Addresses");
+
+            string jsonAddresses = string.Empty;
+            DataRowCollection rows = dataSet.Tables[0].Rows;
+            Address[] addresses = new Address[rows.Count];
+
+            for(int i = 0; i < rows.Count; i++)
+            {
+                DataRow row = rows[i];
+                bool isRegionNotNull = !row.IsNull("Region");
+                string region = string.Empty;
+
+                if(isRegionNotNull)
+                {
+                    region = row.Field<string>("Region");
+                }
+
+                addresses[i] = new Address(row.Field<string>("StreetName"), row.Field<string>("StreetNumber"), row.Field<string>("ZipCode"), row.Field<string>("City"), region, row.Field<string>("Country"));
+            }
+
+            jsonAddresses = JsonConvert.SerializeObject(addresses);
+
+            return jsonAddresses;
+        }
+
+        /// <summary>
+        /// Gets the specified amount of addresses from the Addresses table in the database, and then returns it in JSON format.
+        /// </summary>
+        /// <param name="amount">The amount of addresses to get. If the amount is more than what is in the table, then only that amount will be retreived.</param>
+        /// <returns>Addresses in JSON format</returns>
+        public string GetAddresses(int amount)
+        {
+            SqlExecutor sqlExecutor = new SqlExecutor();
+            DataSet dataSet = sqlExecutor.Execute($"SELECT TOP {amount} * FROM Addresses");
+
+            string jsonAddresses = string.Empty;
+            DataRowCollection rows = dataSet.Tables[0].Rows;
+            Address[] addresses = new Address[rows.Count];
+
+            for(int i = 0; i < rows.Count; i++)
+            {
+                DataRow row = rows[i];
+                bool isRegionNotNull = !row.IsNull("Region");
+                string region = string.Empty;
+
+                if(isRegionNotNull)
+                {
+                    region = row.Field<string>("Region");
+                }
+
+                addresses[i] = new Address(row.Field<string>("StreetName"), row.Field<string>("StreetNumber"), row.Field<string>("ZipCode"), row.Field<string>("City"), region, row.Field<string>("Country"));
+            }
+
+            jsonAddresses = JsonConvert.SerializeObject(addresses);
+
+            return jsonAddresses;
+        }
+    }
+}
